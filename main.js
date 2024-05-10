3c4677279101d8358b4b93b904d73eba667bc4ac
Giôvanni_BS <giovanni.bandeira.sousa@gmail.com>
2024-05-10 20:18:49 -0300

    Demonstração

--- main.js
+++ main.js
@@ -13,8 +13,6 @@ function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
     if(resultado){
         document.getElementById('resultado').innerHTML = eval(resultado);
-    }else{
-        document.getElementById('resultado').innerHTML = "Nada para calcular"
     }
 }
 
